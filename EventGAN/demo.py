78755cb7daba546c8c19ef2ab4c158a564756678
Miracleyoo <mirakuruyoo@gmail.com>
2023-09-06 00:17:30 +0000

    officialize metrics calculation

--- EventGAN/demo.py
+++ EventGAN/demo.py
@@ -1,30 +1,14 @@
 import os.path as op
-# from utils.viz_utils import gen_event_images
 from pytorch_utils import BaseOptions
 from models.eventgan_base import EventGANBase
 import configs
-import cv2
-import numpy as np
 import torch
 import pickle
+import numpy as np
 from tqdm import tqdm
 from pathlib2 import Path
 from utils.event_utils import gen_discretized_event_volume
-# Read in images.
-# prev_image = cv2.imread('EventGAN/example_figs/007203_01.png')
-# next_image = cv2.imread('EventGAN/example_figs/007203.png')
-
-# prev_image = cv2.resize(prev_image, (861, 260))
-# next_image = cv2.resize(next_image, (861, 260))
-
-# prev_image = cv2.cvtColor(prev_image, cv2.COLOR_BGR2GRAY)
-# next_image = cv2.cvtColor(next_image, cv2.COLOR_BGR2GRAY)
-
-# images = np.stack((prev_image, next_image)).astype(np.float32)
-# images = (images / 255. - 0.5) * 2.
-# images = torch.from_numpy(images).cuda()
-
-# Build network.
+from metrics import BinaryMatchF1, BinaryMatch, PoolMSE
 
 options = BaseOptions()
 options.parser = configs.get_args(options.parser)
@@ -32,72 +16,105 @@ args = options.parse_args()
 
 EventGAN = EventGANBase(args)
 
-def dataset_metrics(path, metrics):
+def dataset_metrics(path, results_folder, metrics_dict, metrics_results):
     file=open(path,'rb')
     stem = Path(path).stem
     data=pickle.load(file)
+    
     images=torch.from_numpy(data['images']).float().cuda()
-
     images = images.float()/255.
     images -= 0.5
     images *= 2.
 
-    # print(images.shape)
-    # print(images.dtype)
+    batch_event_voxels_pred = np.zeros((16, args.n_time_bins*2, 260, 346))
+    batch_event_voxels_gt = np.zeros((16, args.n_time_bins*2, 260, 346))
+
     result=[]
     for index in range (images.shape[0]-1):
         current_image=images[index:index+2]
-        #print(current_image.shape)
         event_volume_est = EventGAN.forward(current_image, is_train=False)[0][0]
-        #print(event_volume1[0].shape)
-
         events_truth = data['events'][index]
-
 
         x = torch.tensor(events_truth['x'].copy())
         y = torch.tensor(events_truth['y'].copy())
         t = torch.tensor(events_truth['timestamp'].copy())
-        t=(t-t.min()).float()
+        t = (t-t.min()).float()
         p = torch.tensor(events_truth['polarity'].copy())
 
         events_truth=torch.stack((x,y,t,p),axis=1)
-
         event_volume_truth = gen_discretized_event_volume(events_truth,
                                                         [args.n_time_bins*2,
                                                         260,
                                                         346]).cuda()
         
-        # Save the GT and predicted event volumes.
-        out_info = {
-            'gt_event_volume': event_volume_truth.cpu().numpy(),
-            'gen_event_volume': event_volume_est.cpu().numpy()
-        }
-        results_folder = op.join('.', 'results')
-        Path(results_folder).mkdir(exist_ok=True)
-        out_path = op.join(results_folder, stem + '_{}.pkl'.format(index))
-        with open(out_path, 'wb') as f:
-            pickle.dump(out_info, f)
         
-        result.append([metric.forward(event_volume_est, event_volume_truth) for metric in metrics])
-    return result
+        batch_event_voxels_gt[index] = event_volume_truth.cpu().numpy()
+        batch_event_voxels_pred[index] = event_volume_est.cpu().numpy()
+        
+    # result.append([metric.forward(event_volume_est, event_volume_truth) for metric in metrics])
+    # print(event_volume_est.shape, event_volume_truth.shape)
+    for metric_name, metric in metrics_dict.items():
+        metrics_results[metric_name].append(metric.forward(
+            torch.Tensor(batch_event_voxels_pred).unsqueeze(0), 
+            torch.Tensor(batch_event_voxels_gt).unsqueeze(0)).item()
+        )
+    
+    # Save the GT and predicted event volumes.
+    out_info = {
+        'gt_event_volume': batch_event_voxels_gt,
+        'gen_event_volume': batch_event_voxels_pred,
+        'metrics': {k:v[-1] for k,v in metrics_results.items() if len(v) > 0},
+    }
+    
+    out_path = op.join(results_folder, f'{stem}.pkl')
+    
+    with open(out_path, 'wb') as f:
+        pickle.dump(out_info, f)
+    
+    # return result
 
+metrics_dict = {
+    'BinaryMatchF1_sum_c': BinaryMatchF1(op_type='sum_c'),
+    'BinaryMatchF1_sum_cp': BinaryMatchF1(op_type='sum_cp'),
+    'BinaryMatchF1_raw': BinaryMatchF1(op_type='raw'),
+    'BinaryMatch_sum_c': BinaryMatch(op_type='sum_c'),
+    'BinaryMatch_sum_cp': BinaryMatch(op_type='sum_cp'),
+    'BinaryMatch_raw': BinaryMatch(op_type='raw'),
+    'PoolMSE_2': PoolMSE(kernel_size=2),
+    'PoolMSE_4': PoolMSE(kernel_size=4),
+}
 
-from metrics import BinaryMatchF1,BinaryMatch
+metrics_results = {
+    'BinaryMatchF1_sum_c': [],
+    'BinaryMatchF1_sum_cp': [],
+    'BinaryMatchF1_raw': [],
+    'BinaryMatch_sum_c': [],
+    'BinaryMatch_sum_cp': [],
+    'BinaryMatch_raw': [],
+    'PoolMSE_2': [],
+    'PoolMSE_4': [],
+}
 
-f1=BinaryMatchF1()
-bm=BinaryMatch()
-
-#print(dataset_metrics(r"/tsukimi/datasets/MVSEC/event_chunks_processed/indoor_flying3_data_left-8.pkl",[f1,bm]))
-
+results_folder = op.join('/tsukimi/backup', 'EventGAN-pretrained-model-test-results-new')
+Path(results_folder).mkdir(exist_ok=True)
 info =pickle.load(open(r"/tsukimi/datasets/MVSEC/data_paths.pkl",'rb'))
-result = []
-for file in tqdm(info['val'][100:]):
+
+for file in tqdm(info['test']): #[100:]):
     path = r"/tsukimi/datasets/MVSEC/event_chunks_processed/"+file
-    # print(dataset_metrics(path,[f1,bm]))
-    result += dataset_metrics(path,[f1,bm])
-    print(torch.Tensor(result).mean(0))
+    dataset_metrics(path, results_folder, metrics_dict, metrics_results) #[f1,bm])
+    for metric_name, metric in metrics_dict.items():
+        print(metric_name, metrics_results[metric_name][-1])
     # break
-print(torch.Tensor(result).mean(0))
+
+# save the metrics results
+with open(op.join(results_folder, 'metrics_results.pkl'), 'wb') as f:
+    pickle.dump(metrics_results, f)
+
+for metric_name, metric in metrics_dict.items():
+    print(metric_name, np.mean(metrics_results[metric_name]))
+    
+    
+# print(torch.Tensor(result).mean(0))
 # event_images = gen_event_images(event_volume[-1], 'gen')
 
 # event_image = event_images['gen_event_time_image'][0].cpu().numpy().sum(0)
--- EventGAN/metrics.py
+++ EventGAN/metrics.py
@@ -3,12 +3,8 @@ Metrics implementation for 3D human pose comparisons
 """
 
 import torch
-import importlib
 from torch import nn
-
-
-__all__ = ['Accuracy', 'BinaryMatch', 'BinaryMatchF1', 'MeanRatio']
-
+from einops import rearrange
 
 class BaseMetric(nn.Module):
     def forward(self, pred, gt, gt_mask=None):
@@ -24,7 +20,6 @@ class BaseMetric(nn.Module):
         """
         pass
 
-
 class Accuracy(BaseMetric):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -38,84 +33,93 @@ class Accuracy(BaseMetric):
 
 
 class BinaryMatch(BaseMetric):
-    def __init__(self, **kwargs):
+    def __init__(self, op_type='raw', **kwargs):
         super().__init__(**kwargs)
+        self.op_type = op_type
+        assert self.op_type in ['raw', 'sum_c', 'sum_cp']
 
     def forward(self, pred, y):
+        if self.op_type == 'sum_c':
+            pred = rearrange(pred, 'b l (p c) h w -> b l c p h w', p=2)
+            y = rearrange(y, 'b l (p c) h w -> b l c p h w', p=2)
+            y = torch.sum(y, dim=2)
+            pred = torch.sum(pred, dim=2)
+        elif self.op_type == 'sum_cp':
+            pred = torch.sum(pred, dim=2)
+            y = torch.sum(y, dim=2)
+
         # for all pixels in pred, if pred > 0.01, pred_binary = 1, else 0
+
         pred_binary = torch.where(pred > 0.01, torch.ones_like(pred), torch.zeros_like(pred))
         label_binary = torch.where(y > 0.01, torch.ones_like(y), torch.zeros_like(y))
+        
 
         acc = (pred_binary == label_binary).double().mean()
         return acc
 
+def f1score(pred, y):
+    """
+    Compute F1 score for binary classification
+    Args:
+        pred: predicted voxel
+        y: ground truth voxel
+    Returns:
+        f1: F1 score
+    """
+    TP = (pred * y).sum()
 
-"""
+    # 计算 False Positive (FP)：预测为正类但目标为负类的数量
+    FP = (pred * (1 - y)).sum()
+
+    # 计算 False Negative (FN)：预测为负类但目标为正类的数量
+    FN = ((1 -pred) * y).sum()
+
+    # TN=((1-pred)*(1-y)).sum()
+
+    # 计算 Precision 和 Recall
+    precision = TP / (TP + FP + 1e-8)  # 加上 1e-8 避免除数为 0
+    recall = TP / (TP + FN + 1e-8)
+
+    # 计算 F1 分数
+    f1 = 2 * (precision * recall) / (precision + recall + 1e-8)
+    return f1
+
 class BinaryMatchF1(BaseMetric):
-    def __init__(self, **kwargs) -> None:
-        super().__init__(**kwargs)
-        
-    def forward(self, pred, y):
-        pred_binary = torch.where(pred > 0.01, torch.ones_like(pred), torch.zeros_like(pred))
-        label_binary = torch.where(y > 0.01, torch.ones_like(y), torch.zeros_like(y))
-
-        diff_index = (pred_binary != label_binary)
-        TP = torch.logical_and(label_binary == 1, pred_binary == 1).double().sum()
-        FP = torch.logical_and(label_binary[diff_index] == 0, pred_binary[diff_index] == 1).double().sum()
-        FN = torch.logical_and(label_binary[diff_index] == 1, pred_binary[diff_index] == 0).double().sum()
-        
-        return 2 * TP / (2 * TP + FP + FN)
-"""
-
-class BinaryMatchF1(BaseMetric):
-    def __init__(self, threshold=0.01, **kwargs) -> None:
+    def __init__(self, threshold=0.01, op_type='sum_cp', **kwargs) -> None:
         super().__init__(**kwargs)
         self.threshold = threshold
-        # from torchmetrics.classification import BinaryF1Score
-        try:
-            self.binary_f1_score = importlib.import_module('torchmetrics.functional.classification').binary_f1_score
-        except:
-            # install torchmetrics if not installed
-            subprocess = importlib.import_module('subprocess')
-            subprocess.run(['pip', 'install', 'torchmetrics'])
-            self.binary_f1_score = importlib.import_module('torchmetrics.functional.classification').binary_f1_score
-        
+        self.op_type = op_type
+        assert self.op_type in ['raw', 'sum_c', 'sum_cp']
+
     def forward(self, pred, y):
-        '''
-        y = torch.sum(torch.abs(y),dim=(1,2))
-        pred = torch.sum(torch.abs(pred),dim=(1,2))
-        label_binary = torch.where(y > self.threshold, torch.ones_like(y), torch.zeros_like(y)).to(y.device)
+        if self.op_type == 'sum_c':
+            pred = rearrange(pred, 'b l (p c) h w -> b l c p h w', p=2)
+            y = rearrange(y, 'b l (p c) h w -> b l c p h w', p=2)
+            y = torch.sum(y, dim=2)
+            pred = torch.sum(pred, dim=2)
+        elif self.op_type == 'sum_cp':
+            pred = torch.sum(pred, dim=2)
+            y = torch.sum(y, dim=2)
         
-        f1 = self.binary_f1_score(pred, label_binary, threshold=self.threshold)
-        '''
-        y=torch.sum(y,dim=0)
-        pred=torch.sum(pred,dim=0)
         y = torch.where(y > self.threshold, torch.ones_like(y), torch.zeros_like(y)).to(y.device)
         pred = torch.where(pred > self.threshold, torch.ones_like(pred), torch.zeros_like(pred)).to(pred.device)
-        # 计算 True Positive (TP)：预测为正类且目标为正类的数量
-        TP = (pred * y).sum()
-
-        # 计算 False Positive (FP)：预测为正类但目标为负类的数量
-        FP = (pred * (1 - y)).sum()
-
-        # 计算 False Negative (FN)：预测为负类但目标为正类的数量
-        FN = ((1 -pred) * y).sum()
-
-        TN=((1-pred)*(1-y)).sum()
-        # print('TP:',TP)
-        # print('FP:',FP)
-        # print('FN:',FN)
-        # print('TN:',TN)
-
-        # 计算 Precision 和 Recall
-        precision = TP / (TP + FP + 1e-8)  # 加上 1e-8 避免除数为 0
-        recall = TP / (TP + FN + 1e-8)
-
-
-        # 计算 F1 分数
-        f1 = 2 * (precision * recall) / (precision + recall + 1e-8)
+        
+        f1 = f1score(pred, y)
         return f1
         
+
+class PoolMSE(nn.Module):
+    def __init__(self, kernel_size=2):
+        super(PoolMSE, self).__init__()
+        self.score = nn.MSELoss()
+        self.kernel_size = kernel_size
+        self.pool = nn.AvgPool3d(kernel_size, stride=kernel_size)
+
+    def forward(self, pred, target):
+        pred = rearrange(pred, 'b l (p c) h w -> (b p) (l c) h w', p=2)
+        target = rearrange(target, 'b l (p c) h w -> (b p) (l c) h w', p=2)
+        score = self.score(self.pool(pred), self.pool(target))
+        return score
 
 
 class MeanRatio(BaseMetric):
