519fac051f074c71ba37d44773ec03857a6dc506
ayaka <mirakuruyoo@gmail.com>
2023-09-07 01:47:30 -0700

    thoroughly rewrite the code

--- EventGAN/pytorch_utils/base_options.py
+++ EventGAN/pytorch_utils/base_options.py
@@ -22,7 +22,7 @@ class BaseOptions(object):
         gen = self.parser.add_argument_group('General')
         gen.add_argument('--time_to_run', type=int, default=-1, help='Total time to run in seconds')
         gen.add_argument('--resume', dest='resume', type=str_to_bool, default=True, help='Resume from checkpoint (Use latest checkpoint by default')
-        gen.add_argument('--num_workers', type=int, default=0, help='Number of processes used for data loading')
+        gen.add_argument('--num_workers', type=int, default=8, help='Number of processes used for data loading')
         pin = gen.add_mutually_exclusive_group()
         pin.add_argument('--pin_memory', dest='pin_memory', action='store_true', help='Number of processes used for data loading')
         pin.add_argument('--no_pin_memory', dest='pin_memory', action='store_false', help='Number of processes used for data loading')
@@ -60,23 +60,24 @@ class BaseOptions(object):
         json_path = os.path.join(log_dir, 'config.json')
         if os.path.exists(json_path):
             self.args.from_json = json_path
+        print(self.args.batch_size)
         
-        if self.args.from_json is not None:
-            path_to_json = os.path.abspath(self.args.from_json)
-            with open(path_to_json, "r") as f:
-                json_args = json.load(f)
-            json_args = namedtuple("json_args", json_args.keys())(**json_args)
-            return json_args
-        else:
-            self.args.log_dir = os.path.join(os.path.abspath(self.args.log_dir), self.args.name)
-            self.args.summary_dir = os.path.join(self.args.log_dir, 'tensorboard')
-            if not os.path.exists(self.args.log_dir):
-                os.makedirs(self.args.log_dir)
-            self.args.checkpoint_dir = os.path.join(self.args.log_dir, 'checkpoints')
-            if not os.path.exists(self.args.checkpoint_dir):
-                os.makedirs(self.args.checkpoint_dir)
-            self.save_dump()
-            return self.args
+        # if self.args.from_json is not None:
+        #     path_to_json = os.path.abspath(self.args.from_json)
+        #     with open(path_to_json, "r") as f:
+        #         json_args = json.load(f)
+        #     json_args = namedtuple("json_args", json_args.keys())(**json_args)
+        #     return json_args
+        # else:
+        self.args.log_dir = os.path.join(os.path.abspath(self.args.log_dir), self.args.name)
+        self.args.summary_dir = os.path.join(self.args.log_dir, 'tensorboard')
+        if not os.path.exists(self.args.log_dir):
+            os.makedirs(self.args.log_dir)
+        self.args.checkpoint_dir = os.path.join(self.args.log_dir, 'checkpoints')
+        if not os.path.exists(self.args.checkpoint_dir):
+            os.makedirs(self.args.checkpoint_dir)
+        self.save_dump()
+        return self.args
 
     def save_dump(self):
         if not os.path.exists(self.args.log_dir):
